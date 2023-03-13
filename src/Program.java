7b666c467379d14aeb20258d0a2cbfb412a05163
PriscillaCorreia <primariafc@gmail.com>
2023-03-13 10:01:00 -0300

    Aula basica de if-else prof Nelio

---
+++ src/Program.java
@@ -0,0 +1,21 @@
+
+
+public class Program {
+    public static void main(String[] args) {
+
+       int hora = 20;
+
+       if(hora < 12){
+           System.out.println("Bom dia");
+       }
+       else if( hora < 18){
+               System.out.println("Boa tarde");
+           }
+           else{
+               System.out.println("Boa noite");
+           }
+       }
+
+
+    }
+
